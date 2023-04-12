0ab047baccef91603fc5fd6ab3ce36b84bc41579
vincentvarona <vincentvarona1@gmail.com>
2023-04-12 16:07:06 -0300

    My Portfolio

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector('.header .menu-toggle').addEventListener('click', function() {
+  this.classList.toggle('active');
+});
+
+let prevScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  
+  if (prevScrollPos > currentScrollPos) {
+    document.querySelector('.header').classList.remove('hidden');
+  } else {
+    document.querySelector('.header').classList.add('hidden');
+  }
+  
+  prevScrollPos = currentScrollPos;
+}
+
